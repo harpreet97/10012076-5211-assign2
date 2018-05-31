e05e562283e56be8ecef0b6f10fdaeeb610fc19e
harpreet97 <harrycheema97@yahoo.com>
2018-06-01 00:05:03 +1200

    Q4-LineNo11-I have written Convert.ToInt32 because it can convert any primitive type to a int.

--- question04/Program.cs
+++ question04/Program.cs
@@ -8,9 +8,10 @@ namespace question04
         {
             Console.WriteLine("Print out the reverse timestables (highest number first) for:");
 
-            var number = Console.ReadLine();
+            var number = Convert.ToInt32(Console.ReadLine());
 
-            for(var i = 0; i < 12; i++) {
+            for (var i = 0; i < 12; i++)
+            {
                 var a = i + 1;
                 Console.WriteLine($"{a} x {number} = {a * number}");
             }
