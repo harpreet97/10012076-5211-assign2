7bde5e4f7abfd6ebde02c77b39e3661ee2fd7fd6
harpreet97 <harrycheema97@yahoo.com>
2018-05-31 22:06:37 +1200

    Q3-Line24-Stopwatch is a class in the .NET Framework that is ideal for timing any operation in C# programs so i have created a object of stopwatch for list

--- question03/Program.cs
+++ question03/Program.cs
@@ -10,18 +10,25 @@ namespace question03
         {
             string[] fruitsArray = new string[] { "Banana", "Grapefruit", "Tomato", "Oranges" };
 
-            // List<string> fruitsList = new List<string> { "Banana", "Grapefruit", "Tomato", "Oranges" };
+            List<string> fruitsList = new List<string> { "Banana", "Grapefruit", "Tomato", "Oranges" };
 
             Stopwatch timeForArray = new Stopwatch();
             timeForArray.Start();// Start Method
-            foreach (var fruit in fruitsArray)// Added this foreeach loop within the class to calculate the total time taken by an array.
+            foreach (var fruit in fruitsArray)// Added this foreach loop within the class to calculate the total time taken by an array.
             {
                 Console.WriteLine(fruit);
             }
             timeForArray.Stop();// Stopped the stopwatch
             System.Console.WriteLine($"Total time taken by an array {timeForArray.Elapsed.TotalMilliseconds} ms");// It shows the exact time that it takes to run the array 
 
-
+            Stopwatch timeForList = new Stopwatch();
+            timeForList.Start();// Start Method
+            foreach (var fruit in fruitsList)
+            {
+                Console.WriteLine(fruit);
+            }
+            timeForList.Stop();
+            System.Console.WriteLine(timeForList.Elapsed.TotalMilliseconds);
         }
     }
 }
